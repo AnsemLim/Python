a8c0feefb5e211de9f8a4731a62c1aab4129b087
zhen <ansemlim79@gmail.com>
2021-07-14 20:20:03 +0800

    1.1

---
+++ StringMethod.py
@@ -0,0 +1,31 @@
+name = "ansem lim "
+# calculate the words
+print(len(name))
+
+# calculate the same word
+print(name.count("m"))
+
+# find the word (the code start with 0)
+print(name.find("s"))
+
+# capitalize (but only the first words)
+print(name.capitalize())
+
+# uppercase 
+print(name.upper())
+
+# lowercase 
+print(name.lower())
+
+# digit (if digit is true)
+print(name.isdigit())
+
+# alpha (if no space include is true,else false)
+print(name.isalpha())
+
+# replace character(first is what should be replace,second is what should be replace with)
+print(name.replace("a","i"))
+
+# extra 
+print(name*3)
+
---
+++ multipleassignment.py
@@ -0,0 +1,18 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+# name = "Ansem" 
+# age = 21
+# attractive = True
+
+name, age, attractive = "Ansem", 21 , True
+print(name)
+print(age)
+print(attractive)
+
+# if the data type is same and the value is same
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
